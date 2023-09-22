6042e123432d87b22d1093768b6940e7cc7042f9
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-09-22 18:20:47 +0530

    Create Nth-Fibonacci term.java

---
+++ Nth-Fibonacci term.java
@@ -0,0 +1,53 @@
+import java.util.* ;
+
+import java.io.*; 
+
+public class Solution{
+
+    static final int MOD = 1000000007;
+
+    public static int fibonacciNumber(int n){
+
+        if(n==0){
+
+            return 0;
+
+        }
+
+        if(n<0){
+
+            return 0;
+
+        }
+
+        else{
+
+            int a = 0;
+
+            int b=1;
+
+            int sum = 0;
+
+            for(int i = 1; i<n ;i++){
+
+                sum=a+b;
+
+                a=b;
+
+                b=sum;
+
+                if(b>=MOD){
+
+                    b-=MOD;
+
+                }
+
+            }
+
+            return b;
+
+        }
+
+    }
+
+}
