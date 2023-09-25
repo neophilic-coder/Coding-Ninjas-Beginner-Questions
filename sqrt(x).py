60098a9321d95cddfa9ff0a0637d29ad08afec95
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-09-25 21:38:14 +0530

    Create sqrt(x).py

---
+++ sqrt(x).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        ans=0;
+        if(x>=0):
+            ans =(int)(math.sqrt(x))
+            return ans
+        else:
+            return 0
+
+    
